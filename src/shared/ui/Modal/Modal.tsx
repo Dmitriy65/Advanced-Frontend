89dc82044801baaf84721255f7ff16bdeab50101
dzmitry.razmyslovich <dzmitry.razmyslovich@leverx.com>
2023-02-21 17:40:43 +0300

    add modal component

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,60 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import React, { ReactNode, useEffect, useCallback } from 'react';
+import { Portal } from 'shared/ui/Portal/Portal';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose: () => void;
+}
+
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+    };
+
+    const closeHandler = useCallback(() => {
+        if (onClose) {
+            onClose();
+        }
+    }, [onClose]);
+
+    const contentClickHandler = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    const onKeyDownHandler = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandler();
+        }
+    }, [closeHandler]);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener('keydown', onKeyDownHandler);
+        } else {
+            document.removeEventListener('keydown', onKeyDownHandler);
+        }
+    }, [isOpen, onKeyDownHandler]);
+
+    return (
+        <Portal>
+            <div className={classNames(cls.modal, mods, [className])}>
+                <div className={cls.overlay} onClick={closeHandler}>
+                    <div className={cls.content} onClick={contentClickHandler}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import React, { useState, useCallback } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,11 +12,23 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
 
+    const [authModalOpened, setAuthModalOpened] = useState(false);
+
+    const onToggleModal = useCallback(
+        () => {
+            setAuthModalOpened((prev) => !prev);
+        },
+        [],
+    );
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onToggleModal}>
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={authModalOpened} onClose={onToggleModal}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+            </Modal>
         </div>
     );
 };
