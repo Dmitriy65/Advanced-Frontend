5339c0519d9e0ea77d22bd803ec1d2578814f03c
dzmitry.razmyslovich <dzmitry.razmyslovich@leverx.com>
2023-02-25 14:02:16 +0300

    refactor app theme styles

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -13,6 +13,7 @@ interface ThemeProviderProps {
 const ThemeProvider: FC<ThemeProviderProps> = (props: ThemeProviderProps) => {
     const { children, initialTheme } = props;
     const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
+    document.body.className = theme;
 
     const defaultProps = useMemo(() => ({
         theme,
