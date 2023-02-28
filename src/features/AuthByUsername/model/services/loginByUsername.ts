2336a7cc53654a0f0288860334ffde1faac36d3e
dzmitry.razmyslovich <dzmitry.razmyslovich@leverx.com>
2023-02-28 17:28:21 +0300

    fix axios deps

--- src/features/AuthByUsername/model/services/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername.ts
@@ -1,8 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { LoginScheme } from 'features/AuthByUsername';
-import { User, userActions, userReducer } from 'entities/User';
-import { USER_LOCALSTORAGE_KEY } from 'shared/constants/localStorage';
+import { User, userActions } from 'entities/User';
 
 type AuthDataProps = Pick<LoginScheme, 'username' | 'password'>;
 
