59bd5fb5716c9eed269593e7c0403be6bef0f037
dzmitry.razmyslovich <dzmitry.razmyslovich@leverx.com>
2023-03-12 19:55:01 +0300

    add articles page and article details page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './ArticleDetailsPage.module.scss';
+
+interface ArticleDetailsPageProps {
+    className?: string;
+}
+
+const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            Article Details Page
+        </div>
+    );
+};
+
+export default ArticleDetailsPage;
